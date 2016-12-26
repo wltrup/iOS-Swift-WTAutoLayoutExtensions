b3d4a63b88b3fdc3184ba52fcef6ac4d96a64b9b
Wagner Truppel <trupwl@gmail.com>
2016-12-26 01:53:27 +0000

    Updates to current Swift version

---
+++ Example/Pods/Target Support Files/Pods-WTColorPickerView_Tests/Pods-WTColorPickerView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WTColorPickerView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WTColorPickerView_Tests
+@end
---
+++ Example/Pods/Target Support Files/WTColorPickerView/WTColorPickerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WTColorPickerView : NSObject
+@end
+@implementation PodsDummy_WTColorPickerView
+@end
